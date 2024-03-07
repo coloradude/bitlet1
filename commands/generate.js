edbd68a9f2575f9cd419a26e09b1dd62a599e531
coloradude <steezeven@gmail.com>
2024-03-07 01:00:48 -0700

    added tizen and webos basic sample

--- commands/generate.js
+++ commands/generate.js
@@ -2,22 +2,28 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+import shell from 'shelljs'
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 const createWebTemplate = path => {
   const webHtml = fs.readFileSync(`${__dirname}/../templates/web.html`, 'utf8')
-
   fs.writeFileSync(`./index.html`, webHtml)
-
-  // if (path) {
-  //   console.log('There is a path?')
-  //   fs.writeFileSync(`${path}/index.html`, webHtml)
-  // } else {
-  //   console.log('There is NO PATH')
-  //   fs.writeFileSync(`${__dirname}/index.html`, webHtml)
-  // }
-  
 }
 
-export default createWebTemplate
+const createTizenTemplate = path => {
+  shell.cd('./' || path)
+  shell.exec('git clone https://github.com/bitmovin/bitmovin-player-tizen-demo.git')
+}
+
+const createWebOsTemplate = path => {
+  shell.cd('./' || path)
+  shell.exec('git clone git@github.com:bitmovin/bitmovin-player-webos-demo.git')
+}
+
+export {
+  createWebTemplate,
+  createTizenTemplate,
+  createWebOsTemplate
+}
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 #! /usr/bin/env node
 import { program } from 'commander'
-import createWebTemplate from './commands/generate.js'
+import {
+  createWebTemplate,
+  createTizenTemplate,
+  createWebOsTemplate
+} from './commands/generate.js'
 
 program
     // .command('web <path>')
-    .command('web')
-    .option('-p, --path <path>', "Specify the path where the template will go", '.')
-    .description('Create Bitmovin Web SDK sample project')
-    .action(createWebTemplate)
+  .command('web')
+  .option('-p, --path <path>', "Specify the path where the template will go", '.')
+  .description('Create Bitmovin Web SDK sample project')
+  .action(createWebTemplate)
+
+program
+  .command('tizen')
+  .description('Create Bitmovin Tizen sample project')
+  .action(createTizenTemplate)
+
+program
+  .command('webos')
+  .description('Create Bitmovin WebOS sample project')
+  .action(createWebOsTemplate)
 
 program.parse()
