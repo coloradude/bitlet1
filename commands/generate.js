67f00b3c70634cc53a53bad510ebaf082a93cb3e
coloradude <steezeven@gmail.com>
2024-03-16 19:58:31 -0600

    added roku

--- commands/generate.js
+++ commands/generate.js
@@ -23,13 +23,58 @@ const createWebOSTemplate = path => {
 }
 
 const createiOSTemplate = path => {
+
+  if (fs.existsSync('./bitmovin-player-ios-samples')){
+    fs.rmSync('./bitmovin-player-ios-samples', {recursive: true})
+  }
+
   shell.cd('./' || path)
   shell.exec('git clone https://github.com/bitmovin/bitmovin-player-ios-samples.git')
+  shell.cd('bitmovin-player-ios-samples')
+  shell.exec('pod repo update')
+  shell.exec('pod install')
 }
 
 const createAndroidTemplate = path => {
+
+  if (fs.existsSync('./bitmovin-player-android-samples')){
+    fs.rmSync('./bitmovin-player-android-samples', {recursive: true})
+  }
+
+  
   shell.cd('./' || path)
   shell.exec('git clone https://github.com/bitmovin/bitmovin-player-android-samples.git')
+  shell.cd('bitmovin-player-android-samples')
+
+
+  // Testing how to add configs automagically
+
+  // const extraConfig = `compileOptions {
+  //   sourceCompatibility JavaVersion.VERSION_1_8
+  //   targetCompatibility JavaVersion.VERSION_1_8
+  // }`
+
+
+  // const currentGradle = fs.readFileSync('./build.gradle', 'utf8')
+  // const newGradle = fs.appendFileSync(currentGradle, extraConfig)
+  // console.log(newGradle)
+  // // () => {
+  // //   const gradle = fs.appendFileSync('./bitmovin-player-android-samples/build.gradle', extraConfig)
+  //   console.log(gradle)
+
+  // // shell.exec('./gradlew build')
+}
+
+const createRokuTemplate = path => {
+
+  if (fs.existsSync('./bitmovin-player-roku-samples')){
+    fs.rmSync('./bitmovin-player-roku-samples', {recursive: true})
+  }
+
+  shell.cd('./' || path)
+  shell.exec('git clone git@github.com:bitmovin/bitmovin-player-roku-samples.git')
+  shell.cd('bitmovin-player-roku-samples')
+  
 }
 
 export {
@@ -37,5 +82,6 @@ export {
   createTizenTemplate,
   createWebOSTemplate,
   createiOSTemplate,
-  createAndroidTemplate
+  createAndroidTemplate,
+  createRokuTemplate
 }
